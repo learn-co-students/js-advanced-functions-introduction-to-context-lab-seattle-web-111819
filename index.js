381e241e55a8c762014ca19158de6e72a42cb48f
seanpgallivan <seanpgallivan@hotmail.com>
2020-01-19 01:18:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const createEmployeeRecord = record => {return {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
+}}
+const createEmployeeRecords = records => {
+    let employees = []
+    records.forEach(record => employees.push(createEmployeeRecord(record)))
+    return employees
+}
+const createTimeInEvent = (record, date) => {
+    record.timeInEvents.push({type: "TimeIn", date: date.slice(0,10), hour: parseInt(date.slice(-4))})
+    return record
+}
+const createTimeOutEvent = (record, date) => {
+    record.timeOutEvents.push({type: "TimeOut", date: date.slice(0,10), hour: parseInt(date.slice(-4))})
+    return record
+}
+const hoursWorkedOnDate = (record, date) => {
+    let inTime = record.timeInEvents.reduce((m, event) => event.date == date ? event.hour : m, 0)
+    let outTime = record.timeOutEvents.reduce((m, event) => event.date == date ? event.hour : m, 0)
+    return (outTime - inTime) / 100
+}
+const wagesEarnedOnDate = (record, date) => hoursWorkedOnDate(record, date) * record.payPerHour
+const allWagesFor = record => record.timeInEvents.reduce((m, event) => m + wagesEarnedOnDate(record, event.date), 0)
+const findEmployeeByFirstName = (employees, name) => employees.find(emp => emp.firstName == name)
+const calculatePayroll = employees => employees.reduce((m, emp) => m + allWagesFor(emp), 0)
--- test/indexTest.js
+++ test/indexTest.js
@@ -273,7 +273,7 @@ describe("The payroll system", function () {
       const csvTimesIn = [
         ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
         ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
-        ["Natalia", ["2018-01-03 1700", "2018-01-05 1800", "2018-01-03 1300"]],
+        ["Natalia", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1300"]],
         ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
         ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
         ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
@@ -282,7 +282,7 @@ describe("The payroll system", function () {
       const csvTimesOut = [
         ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
         ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
-        ["Natalia", ["2018-01-03 2300", "2018-01-05 2300", "2018-01-03 2300"]],
+        ["Natalia", ["2018-01-01 2300", "2018-01-02 2300", "2018-01-03 2300"]],
         ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
         ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
         ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
@@ -296,7 +296,7 @@ describe("The payroll system", function () {
             expect(calculatePayroll).to.exist
           })
 
-          it("correctly sums the payroll burden to $11,880 when passed an array of employee records", function () {
+          it("correctly sums the payroll burden to $12,480 when passed an array of employee records", function () {
             let employeeRecords = createEmployeeRecords(csvDataEmployees)
             employeeRecords.forEach(function (rec) {
               let timesInRecordRow = csvTimesIn.find(function (row) {
@@ -315,7 +315,7 @@ describe("The payroll system", function () {
                 createTimeOutEvent(rec, timeOutStamp)
               })
             }) 
-            expect(calculatePayroll(employeeRecords)).to.eql(11880)
+            expect(calculatePayroll(employeeRecords)).to.eql(12480)
           })
         })
       })
